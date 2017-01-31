43c0ff7b6c7836ba778b89d8c0c3f1265a1902b8
individualist-null <micahtk@protonmail.com>
2017-01-31 12:50:02 -0800

    Game is functioning, now move to game object.

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,17 +1,23 @@
 var wordList = ["hello", "goodbye", "fuck"];
 var userInput;
-var guess = 5;
+var guess = 6;
+var miss = 0;
 var dispWord = '';
+//Contains word used for game
 var word = wordList[Math.floor(Math.random() * wordList.length)];
 var guesses = [];
 
 //return index of each matching char/ lese returns -1 if char is not located
+function replaceAt(index, character, string) {
+    return string.substr(0, index) + character + string.substr(index+character.length);
+}
+
 function isInString(char, string) {
     var count = 0;
     var index = [];
 
     for (var i = 0; i < string.length; i++) {
-		if (string.charAt(i) === char) 
+		if (string[i] === char) 
 		{
 			console.log(string[i]);
 			index.push(i);
@@ -25,30 +31,30 @@ function isInString(char, string) {
 	}
 	else
 	{
+		miss++;
 		index.push(-1);
 		return index;
 	}
 }
 
-function setupWord(string){
+//Displays current progress of word
+function setupWord(){
+	//Stores string to be converted into element node.
 	var store;
-	for (var i = 0; i < word.length; i++) {
-		dispWord = dispWord + "_";
-	}
-	store = document.createTextNode(dispWord);
-	document.getElementById('fill').appendChild(store);
-}
+	dispWord = '';
 
-function displayWord(){
-	alert(dispWord);
-	var disp = '';
 	for (var i = 0; i < word.length; i++) {
+		//Fill check place with _
+		dispWord += "_";
 		for(var j = 0; j < guesses.length; j++){
-			if(word.charAt(j) === guesses[i]){
-				disp = disp + guesses[i];
+			//If guesses[n] === to word at current index, replace _ with guess[n]
+			if(word[i] === guesses[j]){
+				dispWord = replaceAt(i, guesses[j], dispWord);
 			}
 		}
 	}
+	store = document.createTextNode(dispWord);
+	document.getElementById('fill').innerHTML = dispWord;
 }
 
 //Displays the user guess to id guessed
@@ -58,22 +64,42 @@ function displayGuess(guess){
 	ref.appendChild(textnode);
 }
 
-function wasGuessed(check){
-	for (var i = 0; i < guesses.length; i++) {
-		if(guesses[i] === check){
-			return true;
-		}
+function displayChances(){
+	var dispChances = "Guesses remaining:";
+
+	for (var i = 0; i < miss; i++) {
+		dispChances += " X";
 	}
-	guesses.push(check);
-	return false;
+	for (var i = 0; i < (guess - miss); i++) {
+		dispChances += " O";
+	}
+	document.getElementById("chances").innerHTML = dispChances;
+	document.getElementById("hanging").src = "assets/images/stage_" + miss + ".png"; 
+}
+
+function wasGuessed(check){
+	if(/^[a-zA-Z]+$/.test(check) && check.length === 1)
+	{
+		for (var i = 0; i < guesses.length; i++) {
+			if(guesses[i] === check){
+				return true;
+			}
+		}
+		guesses.push(check);
+		return false;
+	}
+	return true;
 }
 
 setupWord(word);		
 document.onkeyup = function(event) {
-var userGuess = event.key;
+var userGuess = event.key.toLowerCase();
+
 if(!wasGuessed(userGuess)){
 	displayGuess(userGuess);
 	alert("The word is: " + word + "\n The guess was: " + userGuess + "\nThe guess was " + isInString(userGuess, word).toString());
+	setupWord();
+	displayChances();
 }
 
 
